716506c97963e4d8340ba6b01d6adb38ea2c6fb6
Bitthal Ladiwal <bitthal.ladiwal@gmail.com>
2022-02-13 02:50:44 -0800

    uploaded files

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get -y install python3-pip
+sudo apt-get install python3-tk
+sudo pip3 install numpy
+sudo pip3 install gTTS~=2.2.3
+sudo pip3 install sounddevice~=0.4.4
+sudo apt-get install libportaudio2
+sudo pip3 install SoundFile~=0.10.3.post1
+sudo pip3 install SpeechRecognition
+sudo pip3 install playsound
